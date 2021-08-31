54380c4f5280b4cf75492c23693b40808a2cbbd2
Tarmity <tarmity.flassman@gmail.com>
2021-08-31 17:59:42 +1000

    doing the fetch calls for the api

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // DEPENDENICES 
 // Series of npm packages that will be used to give the server functionality. 
 const express = require('express');
+const axios = require('axios')
+const fetch = require('node-fetch');
+const { response } = require('express');
 
 // EXPRESS CONFIGURATION
 // Tells node that we are creating an "express" server.
@@ -13,9 +16,36 @@ const PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Router
-// Require and point the server to a series of route files.
-require("./routes/apiRoutes")(app);
-
 // The below code starts the server.
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
+
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+// fetch call for the todo's 
+// let todos;
+async function catchTodos() {
+    const response = await fetch (url);
+    const todos = await response.json();
+    document.getElementById('data').src = todos
+    return todos
+}
+
+ const todoList = catchTodos()
+
+console.log(todoList)
+
+// const todos = fetch(url)
+// .then(response => {
+//     console.log(response)
+//     return response.json()
+// })
+// .then(response => {
+//   console.log(response)
+//   todoList = response
+// })
+// // .then(data => todos = data)
+// // .then(todos.json)
+// let todoList = todos;
+// let userList;
+// console.log(`This is the ${todoList}`)
+// console.log(`WELL THIS IS THE TODOLIST ${userList}`)
