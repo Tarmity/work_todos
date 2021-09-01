f88bc75e2a5ac727ae68c7cb0de27a827b51152b
Tarmity <tarmity.flassman@gmail.com>
2021-09-01 11:41:53 +1000

    created the test file for the server.js file

--- server.js
+++ server.js
@@ -1,71 +1,72 @@
-// DEPENDENICES 
-// // Series of npm packages that will be used to give the server functionality. 
+// DEPENDENICES
+// // Series of npm packages that will be used to give the server functionality.
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 // JSONPlaceholder API urls
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
-const todosUrl = 'https://jsonplaceholder.typicode.com/todos'
+const userUrl = "https://jsonplaceholder.typicode.com/users";
+const todosUrl = "https://jsonplaceholder.typicode.com/todos";
 
-// fetch call for the users and there id 
+// fetch call for the users and there id
 async function getUsers() {
-    const response = await fetch (userUrl);
-    const users = await response.json();
-    return users
+  const response = await fetch(userUrl);
+  const users = await response.json();
+  return users;
 }
 
-// async function to fetch todos 
+module.exports.getUsers = getUsers;
+
+// async function to fetch todos
 async function getTodos() {
-    const response = await fetch (todosUrl);
-    const todos = await response.json();
-    return todos
+  const response = await fetch(todosUrl);
+  const todos = await response.json();
+  return todos;
 }
 
-// async function to get User Todos  
+// async function to get User Todos
 async function getUsersTodos(username) {
-    try {
-        let users = await getUsers();
-        const user = users.find(u => u.username === username)
-        if (!user) {
-            throw new Error(`User with username of ${username} was not found in users list`)
-        }
-        
-        const todos = await getTodos();
-
-        let outstanding = 0;
-        let total = 0
-        let jobTitle = []
-       
-        
-        todos.forEach(todo => {
-            if (todo.userId === user.id) {
-                total++
-            
-                if((todo.completed === false)){
-                    outstanding++
-                    jobTitle.push(todo.title)
-                }
-            }
-        })
-
-        console.log(`${username} has ${total} tasks of which ${outstanding} are uncompleted, \n Current outstanding jobs are:`)
-        //Loop to print the outstanding jobs to the console
-        jobTitle.forEach(job => {
-            let jobs = `- ${job}`;
-            console.log(jobs)
-       })
-       
-    } catch (err) {
-        console.error(err)
+  try {
+    let users = await getUsers();
+    const user = users.find((u) => u.username === username);
+    if (!user) {
+      throw new Error(
+        `User with username of ${username} was not found in users list`
+      );
     }
+
+    const todos = await getTodos();
+
+    let outstanding = 0;
+    let total = 0;
+    let jobTitle = [];
+
+    todos.forEach((todo) => {
+      if (todo.userId === user.id) {
+        total++;
+
+        if (todo.completed === false) {
+          outstanding++;
+          jobTitle.push(todo.title);
+        }
+      }
+    });
+
+    console.log(
+      `${username} has ${total} tasks of which ${outstanding} are uncompleted, \n Current outstanding jobs are:`
+    );
+    //Loop to print the outstanding jobs to the console
+    jobTitle.forEach((job) => {
+      let jobs = `- ${job}`;
+      console.log(jobs);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-
-const input = process.argv[2]
+const input = process.argv[2];
 if (!input) {
-    console.error("A username argument is required")
+  console.error("A username argument is required");
 } else {
-    getUsersTodos(input)
+  getUsersTodos(input);
 }
-
-
