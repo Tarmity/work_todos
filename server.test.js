5fe39978d0a1e064bdaa6c9dbe8622b935492827
Tarmity <tarmity.flassman@gmail.com>
2021-09-01 12:14:26 +1000

    created the test for the getTodos function

--- server.test.js
+++ server.test.js
@@ -1,7 +1,14 @@
 const { expect } = require('@jest/globals');
 const { getUsers } = require('./server');
+const { getTodos } = require('./server');
 
-test("Should out put JSON Data", () => {
+
+test("Should output JSON Data", () => {
     const JSON = getUsers({JSON: true});
+    expect(JSON)
+})
+
+test("Should output JSON Data", () => {
+    const JSON = getTodos({JSON: true});
     expect(JSON)
 })
